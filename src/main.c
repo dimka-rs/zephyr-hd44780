09a42c4c3708bea33e3c24ab384bca146f3aced8
Dmitry K <dimka.rs@gmail.com>
2019-11-19 23:13:15 +0300

    led: removed, shares pin with LCD

--- src/main.c
+++ src/main.c
@@ -17,35 +17,24 @@ void main(void)
     u32_t val = 0, i;
     printk("start app\n");
 
-    led_init();
     button_init();
     hd44780_init();
 
-    while (1) {
-        printk("LED %d\n", val);
-        if (val > 0)
+    while (1)
+    {
+        hd44780_cmd(HD44780_CMD_CLEAR, 0);
+
+        hd44780_pos(0, 0);
+        for(i = 0; i < sizeof(str1) - 1; i++)
         {
-            len_off();
-            val = 0;
-        }
-        else
-        {
-            len_on();
-            val = 1;
+            hd44780_data(str1[i]);
         }
 
-    hd44780_cmd(HD44780_CMD_CLEAR, 0);
-    hd44780_pos(0, 0);
-    for(i = 0; i < sizeof(str1) - 1; i++)
-    {
-        hd44780_data(str1[i]);
-    }
-
-    hd44780_pos(1, 0);
-    for(i = 0; i < sizeof(str2) - 1; i++)
-    {
-        hd44780_data(str2[i]);
-    }
+        hd44780_pos(1, 0);
+        for(i = 0; i < sizeof(str2) - 1; i++)
+        {
+            hd44780_data(str2[i]);
+        }
         k_sleep(SLEEPTIME);
     }
 }
